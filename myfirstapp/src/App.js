a030e787aeb878dee0fab7ea67aca349796bd8d7
Eden Smith <edenwint@gmail.com>
2020-09-24 20:53:14 +0100

    bookstore added

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // inline style
+  const h1style = {"fontSize": "100px", "color": "#a6b1e1", "textDecoration": "underline", "textDecoration": "uppercase"}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* inline style */}
+        <h1 style= {h1style}> 
+         This is my first app!
+        </h1>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
