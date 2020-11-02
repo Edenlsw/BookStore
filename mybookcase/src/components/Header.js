1d231fe405765557729157603f8f0f4b7730d102
Eden Smith <edenwint@gmail.com>
2020-11-02 02:41:20 +0000

    bootstrap installed

--- mybookcase/src/components/Header.js
+++ mybookcase/src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 // import About from '../pages/About';
+import Navbar from 'react-bootstrap/Navbar';
 import './header.css';
+
 
 
 const Header = () => {
     return (
         <React.Fragment>
             <div className="title-banner">
-                
-                <h1 className="title"> WELCOME</h1>
+
+                <h1 className="title"> Eden's BookStore</h1>
                 <div className="header">
                     
                     
